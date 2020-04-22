a9f958bfea5a62b12fcf5620ece7582c77f144c2
xlyan <xlyan@vova.com.hk>
2020-04-22 20:00:48 +0800

    interface update

--- index.d.ts
+++ index.d.ts
@@ -1,34 +1,36 @@
-import * as sequelize from 'sequelize';
-
-interface EggSequelizeOptions extends sequelize.Options {
-  /**
-   * load all models to `app[delegate]` and `ctx[delegate]`, default to `model`
-   */
-  delegate?: string;
-
-  /**
-   * load models from `app/model/*.js`
-   */
-  baseDir?: string;
-
-  /**
-   * ignore `app/${baseDir}/index.js` when load models, support glob and array
-   */
-  exclude?: string | Array<string>;
-
-  /**
-   * A full database URI
-   * @example
-   * `connectionUri:"mysql://localhost:3306/database"`
-   */
-  connectionUri?: string;
-}
-
-interface DataSources {
-  datasources: EggSequelizeOptions[];
-}
-
 declare module 'egg' {
+  import * as sequelize from 'sequelize';
+
+  interface EggSequelizeOptions extends sequelize.Options {
+    /**
+     * load all models to `app[delegate]` and `ctx[delegate]`, default to `model`
+     */
+    delegate?: string;
+
+    /**
+     * load models from `app/model/*.js`
+     */
+    baseDir?: string;
+
+    /**
+     * ignore `app/${baseDir}/index.js` when load models, support glob and array
+     */
+    exclude?: string | Array<string>;
+
+    /**
+     * A full database URI
+     * @example
+     * `connectionUri:"mysql://localhost:3306/database"`
+     */
+    connectionUri?: string;
+  }
+
+  interface DataSources {
+    datasources: EggSequelizeOptions[];
+  }
+
+  type PlainObject<T = any> = { [key: string]: T };
+
   interface IModel extends sequelize.Sequelize, PlainObject {}
 
   // extend app
